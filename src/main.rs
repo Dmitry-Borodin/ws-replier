e3b1f7ef214ca22befe49c3b9d9044947a4f5e39
Dmitry Borodin <git@DmitryBorodin.com>
2024-06-19 23:19:42 +0200

    init requested

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use tungstenite::{connect, Message};
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let (mut socket, response) = connect("ws://34.159.18.133:22430/").expect("Can't connect");
+
+    println!("Connected to the server");
+    println!("Response HTTP code: {}", response.status());
+    println!("Response contains the following headers:");
+
+    loop {
+        let message = socket.read().expect("Error reading message");
+        // println!("Received: {:?}", message.into_data());
+
+        match message {
+            msg @ Message::Text(_) => {
+                socket.send(msg)?;
+            }
+            msg @ Message::Binary(_) => {
+                socket.send(msg)?;
+            }
+            Message::Ping(_) | Message::Pong(_) | Message::Close(_) | Message::Frame(_) => {}
+        }
+    }
+}
+
+fn get_reply(challenge: Vec<u8>) -> Result<Vec<u8>> {
+
 }
